2b0efefc52d791b885e663c78ebd154a0d65892b
Jake Knecht <Ljknecht91@gmail.com>
2019-08-23 10:31:18 -0500

    add sql transaction to GET route

--- server/routes/account.router.js
+++ server/routes/account.router.js
@@ -4,7 +4,18 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
-  res.send('Hello?');
+
+  pool.query(`Select name, SUM(transaction.amount) from account
+	join transaction on account.id = transaction.acct_id
+  GROUP BY name;`)
+  .then(result => {
+    res.send(result.rows);
+  })
+  .catch( error=>{
+    res.sendStatus(500);
+    console.log('oops, error occured:', error);
+    
+  })
 })
 
 module.exports = router;
